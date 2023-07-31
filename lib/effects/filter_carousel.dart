a865a680ef5e22dfde173f74320bde01bf68bb3c
teamkotlin <muhammadramzan6254@gmail.com>
2023-07-31 06:26:07 +0500

    carousel

---
+++ lib/effects/filter_carousel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class FilterCarosousel extends StatelessWidget {
+  const FilterCarosousel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
